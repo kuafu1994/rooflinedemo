dc7df7c9f6913615475a7cc2843d7bb8d8f1339e
zhangpengfei <1155100740@link.cuhk.edu.hk>
2019-09-26 17:17:11 +0800

    Update matmul.c

--- matmul.c
+++ matmul.c
@@ -1,37 +1,38 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-#define M 256
-#define K 512
-#define N 256
 int main(){
+    int M = 1024;
+    int K = 512;
+    int N = 1024;
     
-    float A[M][K];
-    float B[K][N];
-    float C[M][N];
+    int* A = (int*) malloc(M * K * sizeof(int));
+    int* B = (int*) malloc(K * N * sizeof(int));
+    int* C = (int*) malloc(M * N * sizeof(int));
     
-    for(int i = 0; i < M; i++)
-        for(int j = 0; j < K; j++)
-            A[i][j] = rand() / RAND_MAX;
+    for(int i = 0; i < M * K; i++)
+        A[i] = 1;
 
-    for(int i = 0; i < K; i++)
-        for(int j = 0; j < N; j++)
-            B[i][j] = rand() / RAND_MAX;
+    for(int i = 0; i < K * N; i++)
+        B[i] = 1;
 
-    for(int i = 0; i < M; i++)
-        for(int j = 0; j < N; j++)
-            C[i][j] = 0.0;
+    for(int i = 0; i < M * N; i++)
+        C[i] = 0;
     
     printf("Start matrix multiplication...\n");
     for(int m = 0; m < M; m++){
         for(int n = 0; n < N; n++){
             for(int k = 0; k < K; k++){
-                C[m][n] += A[m][k] * B[k][n];
+                C[m * N + n] += A[m * K + k] * B[k * N + n]; 
             }
         }
     }   
     printf("End matrix multiplication...\n");
+    
+    int total = 0;
+    for(int i = 0; i < M*N; i ++) 
+        total += C[i];
 
+    printf("total=%d\n", total);
     return 0;
 }
 
